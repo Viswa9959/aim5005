2eaf091423827bcf02b4abf25fba80dc0e7563d2
Viswa9959 <vkamired@mail.yu.edu>
2024-09-30 19:56:49 -0400

    Update features.py

--- aim5005/features.py
+++ aim5005/features.py
@@ -37,9 +37,73 @@ class MinMaxScaler:
         x = self._check_is_array(x)
         self.fit(x)
         return self.transform(x)
-    
-    
+
 class StandardScaler:
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.std = None
+        
+    def _check_is_array(self, x: Union[List, np.ndarray]) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it's not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+        
+    def fit(self, x: Union[List, np.ndarray]) -> None:   
+        x = self._check_is_array(x)
+        self.mean = np.mean(x, axis=0)
+        self.std = np.std(x, axis=0)
+        
+    def transform(self, x: Union[List, np.ndarray]) -> np.ndarray:
+        """
+        Standardize the given vector
+        """
+        x = self._check_is_array(x)
+        return (x - self.mean) / self.std
+    
+    def fit_transform(self, x: List) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
+
+class LabelEncoder:
+    def __init__(self):
+        self.classes_ = None
+        self.class_to_index: Dict[Any, int] = {}
+        
+    def _check_is_array(self, y: Union[List, np.ndarray]) -> np.ndarray:
+        """
+        Try to convert y to a np.ndarray if it's not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(y, np.ndarray):
+            y = np.array(y)
+            
+        assert isinstance(y, np.ndarray), "Expected the input to be a list"
+        return y
+        
+    def fit(self, y: Union[List, np.ndarray]) -> None:
+        """
+        Fit the LabelEncoder to the given labels
+        """
+        y = self._check_is_array(y)
+        unique_classes = sorted(set(y))
+        self.classes_ = np.array(unique_classes)
+        self.class_to_index = {cls: idx for idx, cls in enumerate(self.classes_)}
+        
+    def transform(self, y: Union[List, np.ndarray]) -> np.ndarray:
+        """
+        Transform labels to normalized encoding
+        """
+        y = self._check_is_array(y)
+        return np.array([self.class_to_index[item] for item in y])
+    
+    def fit_transform(self, y: Union[List, np.ndarray]) -> np.ndarray:
+        """
+        Fit the LabelEncoder and transform the given labels
+        """
+        self.fit(y)
+        return self.transform(y)

ebfc94b7b14b8151a7a7db2f0a72bc324cc563ad
Viswa9959 <vkamired@mail.yu.edu>
2024-09-30 20:53:58 -0400

    Update test_features.py

--- tests/test_features.py
+++ tests/test_features.py
@@ -62,29 +62,16 @@ class TestFeatures(TestCase):
         result = scaler.transform([[2., 2.]]) 
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
-    def test_label_encoder_different_categories(self):
-        # Sample categorical data with different categories
-        categories = ['apple', 'banana', 'orange', 'apple', 'orange', 'banana', 'kiwi']
-        expected_classes = np.array(['apple', 'banana', 'kiwi', 'orange'])  # Sorted unique classes
-        expected_encoded = np.array([0, 1, 3, 0, 3, 1, 2])  # Encoded values
-    
-        # Create LabelEncoder instance
-        encoder = LabelEncoder()
-        
-        # Fit the encoder
-        encoder.fit(categories)
-    
-        # Check the classes_ attribute
-        assert (encoder.classes_ == expected_classes).all(), "Classes do not match expected values."
-    
-        # Transform the categories
-        result_encoded = encoder.transform(categories)
-        
-        # Validate the encoding
-        assert (np.array(result_encoded) == expected_encoded).all(), "Encoding does not match expected values. Expect {}. Got: {}".format(expected_encoded, result_encoded)
-    
+    def test_label_encoder_single_value(self):
+        data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+        expected = np.array([[3., 3.]])
+        encoder= LabelEncoder()
+        encoder.fit(data)
+        result = encoder.transform([[2., 2.]]) 
+        assert (result == expected).all(), "encoder transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
-    
+
+
     
 if __name__ == '__main__':
     unittest.main()
